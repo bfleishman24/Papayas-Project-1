c597826bfa1f6478f67c0b729d70f1c0fcf93e21
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2018-01-13 10:45:20 -0600

    adding firebase

---
+++ assets/Javascript/app.js
@@ -0,0 +1,16 @@
+$('document').ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: 'AIzaSyCacnSxWt5028xJ6wkWR-1KeHNFbc8Ytws',
+        authDomain: 'papayas-project-1.firebaseapp.com',
+        databaseURL: 'https://papayas-project-1.firebaseio.com',
+        projectId: 'papayas-project-1',
+        storageBucket: 'papayas-project-1.appspot.com',
+        messagingSenderId: '795888444944',
+    };
+    firebase.initializeApp(config);
+
+
+
+
+});
