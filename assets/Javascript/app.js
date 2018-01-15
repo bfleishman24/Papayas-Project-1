fb8374b472322571a9b426031a372f38c23d859d
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2018-01-15 13:22:50 -0600

    Found Notation for Spotify

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -10,34 +10,26 @@ $('document').ready(function() {
     };
     firebase.initializeApp(config);
 
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://api.spotify.com/v1/browse/categories/dinner/playlists?country=US&limit=1&offset=5',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer BQBFsp8aA4OW05H8vjU4rT4EACqX93BUxnopleEVU528T7RhgwoWb5krLy2OzqFy-Php45c1eZd5ghT7rlnSaYYsdiP-mUdvgJ34FFbPJC1vhGbsHJktmXW3ku5wv1b0Ra3tSmR5DYA17J3Krg'
-        },
-    };
+    var settings = { async: true, crossDomain: true, url: 'https://api.spotify.com/v1/browse/categories/dinner/playlists?country=US&limit=1&offset=5', method: 'GET', headers: { 'Content-Type': 'application/json', Authorization: 'Bearer BQBZ-Jd9BEaudBsskaabCOfWElj3KOlMUbafIrEFxeC1cdjlolhvr0PMBYFcLgMg0SBmpCoiyvQaBpvwgEiiKoU2cAOSz8eKKTG7iV8FWMF0hMYatjYDvAjeFLkiH_uh_A2J-v8choLz56NOsA' } };
 
     $.ajax(settings).done(function(response) {
 
         console.log(response);
-
+        console.log(JSON.stringify(response));
+        console.log(response.playlists.items[0].external_urls.spotify);
         var results = response.playlists.items.external_urls;
 
-        console.log(results);
+        //console.log(results);
 
 
         //for (var i = 0; i < results.length; i++) {
         // Creating a div to hold the Playlists
-        //var playlistDiv = $("<div class='player'>");
+        var playlistDiv = $("<div class='player'>");
         //var resultPlaylist = JSON.stringify(results[i]);
 
         // Putting the entire Playlist above the previous Playlists
-        //playlistDiv.prepend(resultPlaylist);
-        $('.playlist').prepend(response);
+        //playlistDiv.prepend(response.playlists.items[0].external_urls.spotify);
+        $('.playlist').prepend(response.playlists.items[0].external_urls.spotify);
 
         //}
 
