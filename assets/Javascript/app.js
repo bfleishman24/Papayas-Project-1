381603d087640d749395c0687469486c0caa85cb
Peter Boss <m@Peters-MacBook-Pro.local>
2018-01-20 12:41:13 -0600

    fixed the genres

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -47,174 +47,163 @@ $('document').ready(function() {
                 }
         });
     });
+
   
-var movieGenres = [{
-    "id": 28,
-    "name": "Action"
-    },
-    {
-    "id": 12,
-    "name": "Adventure"
-    },
-    {
-    "id": 16,
-    "name": "Animation"
-    },
-    {
-    "id": 35,
-    "name": "Comedy"
-    },
-    {
-    "id": 80,
-    "name": "Crime"
-    },
-    {
-    "id": 99,
-    "name": "Documentary"
-    },
-    {
-    "id": 18,
-    "name": "Drama"
-    },
-    {
-    "id": 10751,
-    "name": "Family"
-    },
-    {
-    "id": 14,
-    "name": "Fantasy"
-    },
-    {
-    "id": 36,
-    "name": "History"
-    },
-    {
-    "id": 27,
-    "name": "Horror"
-    },
-    {
-    "id": 10402,
-    "name": "Music"
-    },
-    {
-    "id": 9648,
-    "name": "Mystery"
-    },
-    {
-    "id": 10749,
-    "name": "Romance"
-    },
-    {
-    "id": 878,
-    "name": "Science Fiction"
-    },
-    {
-    "id": 10770,
-    "name": "TV Movie"
-    },
-    {
-    "id": 53,
-    "name": "Thriller"
-    },
-    {
-    "id": 10752,
-    "name": "War"
-    },
-    {
-    "id": 37,
-    "name": "Western"
-    }
-    ];
-    console.log("helo")
+    var movieGenres = [{
+        "id": 28,
+        "name": "Action"
+        },
+        {
+        "id": 12,
+        "name": "Adventure"
+        },
+        {
+        "id": 16,
+        "name": "Animation"
+        },
+        {
+        "id": 35,
+        "name": "Comedy"
+        },
+        {
+        "id": 80,
+        "name": "Crime"
+        },
+        {
+        "id": 99,
+        "name": "Documentary"
+        },
+        {
+        "id": 18,
+        "name": "Drama"
+        },
+        {
+        "id": 10751,
+        "name": "Family"
+        },
+        {
+        "id": 14,
+        "name": "Fantasy"
+        },
+        {
+        "id": 36,
+        "name": "History"
+        },
+        {
+        "id": 27,
+        "name": "Horror"
+        },
+        {
+        "id": 10402,
+        "name": "Music"
+        },
+        {
+        "id": 9648,
+        "name": "Mystery"
+        },
+        {
+        "id": 10749,
+        "name": "Romance"
+        },
+        {
+        "id": 878,
+        "name": "Science Fiction"
+        },
+        {
+        "id": 10770,
+        "name": "TV Movie"
+        },
+        {
+        "id": 53,
+        "name": "Thriller"
+        },
+        {
+        "id": 10752,
+        "name": "War"
+        },
+        {
+        "id": 37,
+        "name": "Western"
+        }
+        ];
 
 // creating a for loop to run throug all the genre ids
 // once we have each genre seperated from the id we will append the genre name to the dropdown
     for (i = 0; i < movieGenres.length ; i++) {
         var genreName=movieGenres[i]
         var newLi= $("<button>");
-            newLi.addClass("dropdown-item").attr("value", genreName.id).text(genreName.name);
-            $('.dropdown-menu').append(newLi);
-            console.log(movieGenres[i])
-
-
-        
-
-    }
-    
+            newLi.addClass("dropdown-item genre-dropdown").attr("value", genreName.id).html(genreName.name);
+            $('.genre-list').append(newLi);
+    };
 
 // function displayMovieInfo() {
     $('.dropdown-item').on('click', function(event) {
         event.preventDefault();
+        $("#title").html("");
+        $("#movieP").html("");
+        $("#plot").html("");
 
-    $("#title").html("");
-    $("#movieP").html("");
-    $("#plot").html("");
-
-
-
-    var genre = $(this).attr("value");
-    console.log("This worked!");
-    var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=13fb7053b108732df8330eeb99f8a1f2&page=1&with_genres="+ genre +"&primary_release_date.lte=2016-1-1";
-    // var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=13fb7053b108732df8330eeb99f8a1f2&page=1&with_genres=28&primary_release_date.lte=2016-1-1";
+        var genre = $(this).attr("value");
+        var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=13fb7053b108732df8330eeb99f8a1f2&page=1&with_genres="+ genre +"&primary_release_date.lte=2016-1-1";
+        // var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=13fb7053b108732df8330eeb99f8a1f2&page=1&with_genres=28&primary_release_date.lte=2016-1-1";
 
 
-//     // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-        console.log("this is working")
-        console.log(response);
-        // console.log(JSON.stringify(response));
-        console.log(response.results[0].poster_path);;
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).done(function(response) {
+            console.log("this is working")
+            console.log(response);
+            // console.log(JSON.stringify(response));
+            console.log(response.results[0].poster_path);;
 
 
-        var randomMovie = Math.floor(Math.random() * response.results.length);
+            var randomMovie = Math.floor(Math.random() * response.results.length);
 
-        console.log(randomMovie);
-
-
-        var movieTitle = response.results[randomMovie].original_title;
-        console.log(movieTitle);
-
-        var movieI = response.results[randomMovie].poster_path;
-        console.log(movieI);
-
-        var imgaeURL = "http://image.tmdb.org/t/p/w185/";
-
-        var imgURL= imgaeURL + movieI;
-
-        console.log(imgURL);
-
-        var moviePlot = response.results[randomMovie].overview;
-        console.log(moviePlot);
+            console.log(randomMovie);
 
 
-    //   // Creating a div to hold the movie title
-        
-        var title = $("<h1>").text(movieTitle);
+            var movieTitle = response.results[randomMovie].original_title;
+            console.log(movieTitle);
 
-        // creating a imge to store the movie photo
+            var movieI = response.results[randomMovie].poster_path;
+            console.log(movieI);
 
-        var image = $("<img>").attr("src", imgURL);
+            var imgaeURL = "http://image.tmdb.org/t/p/w185/";
 
-        //creating a p to hold the movie plot
+            var imgURL= imgaeURL + movieI;
 
-        var plot = $("<p>").text(moviePlot);
+            console.log(imgURL);
 
-        // prepending all the movie data to the page
+            var moviePlot = response.results[randomMovie].overview;
+            console.log(moviePlot);
 
-        $("#title").prepend(title);
 
-        $("#movieP").prepend(image);
+        //   // Creating a div to hold the movie title
+            
+            var title = $("<h1>").text(movieTitle);
 
-        $("#plot").prepend(plot);
+            // creating a imge to store the movie photo
+
+            var image = $("<img>").attr("src", imgURL);
+
+            //creating a p to hold the movie plot
+
+            var plot = $("<p>").text(moviePlot);
+
+            // prepending all the movie data to the page
+
+            $("#title").prepend(title);
+
+            $("#movieP").prepend(image);
+
+            $("#plot").prepend(plot);
 
 
     });
 
-  });
-/*
+    /*
                 var ingredients = recipe.extendedIngredients;
                 for (i = 0; i < ingredients.length; i++) {
                     $(".ingredients").append(ingredients[i].originalString);
@@ -224,4 +213,5 @@ var movieGenres = [{
                     $(".instructions").append(instructions[j].step);
               
 */
-
+  });
+})
