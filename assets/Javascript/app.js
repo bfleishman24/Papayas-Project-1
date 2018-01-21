168af41dcb69ed7c41151fe5954c8a7f70d433c0
Mike <mike@DT03mMI01.local>
2018-01-20 21:51:30 -0600

    all working so far

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -8,65 +8,23 @@ $('document').ready(function() {
     'party',
   ];
 
+  for (i = 0; i < playlistCategories.length; i++) {
+      var playlistName = playlistCategories[i];
+      //console.log(playlistName);
+      var newPlaylist = $('<div>');
+      newPlaylist
+        .addClass('dropdown-item playlist-dropdown')
+        .attr('value', playlistName)
+        .html(playlistName);
+      $('.playlist-list').append(newPlaylist);
+      //console.log(newPlaylist);
+    }
 
   $('.dropdown-toggle').on('click', function(event) {
     event.preventDefault();
-        for (i = 0; i < playlistCategories.length; i++) {
-          var playlistName = playlistCategories[i];
-          console.log(playlistName);
-          var newPlaylist = $('<button>');
-          newPlaylist
-            .addClass('dropdown-item playlist-dropdown')
-            .attr('value', playlistName)
-            .html(playlistName);
-          $('.playlist-list').append(newPlaylist);
-          console.log(newPlaylist);
-        }
-    console.log('Dropdown Clicked');
-    $('#title').html('');
-    $('#movieP').html('');
-    $('#plot').html('');
+    $('.playlist').html('');
 
-    var genre = $(this).attr('value');
-    /*
-  for (i = 0; i < playlistCategories.length; i++) {
-        var playlistName=playlistCategories[i]
-        var newMood= $("<div>");
-            newMood
-              .addClass('dropdown-item mood-dropdown')
-              .attr('value', playlistName)
-              .html(playlistName);
-            $('.playlist-lits').append(newMood);
-    console.log(playlistName);
-    console.log(newMood);
-  };*/
-
-    // creating a for loop to run throug all the genre ids
-    // once we have each genre seperated from the id we will append the genre name to the dropdown
-    /*for (i = 0; i < movieGenres.length ; i++) {
-        var genreName=movieGenres[i]
-        var newLi= $("<button>");
-            newLi.addClass("dropdown-item genre-dropdown").attr("value", genreName.id).html(genreName.name);
-            $('.genre-list').append(newLi);
-    };*/
-
-    /*$('.dropdown-toggle').on('click', function(event) {
-        event.preventDefault();
-        console.log('clicked');
-        $('.playlist-list').html(playlistCategories);
-
-        var genre = $(this).attr('value');
-        console.log(genre);
-      });*/
-
-    // function displayMovieInfo() {
-    /*$('.dropdown-item').on('click', function(event) {
-        event.preventDefault();
-        $("#title").html("");
-        $("#movieP").html("");
-        $("#plot").html("");
-
-        var genre = $(this).attr("value");*/
+    //console.log('Dropdown Clicked');
 
     var authSettings = {
       async: true,
@@ -76,7 +34,8 @@ $('document').ready(function() {
       headers: {
         Authorization: 'Basic NjIyYTUwNDZkN2ZkNDM2Njk4MGNhZDgyNDJiNTYzY2U6ZjA2YWNhNzUxNGI5NDI1ZDk3MTc3MGIzMDMwNGZkMzE=',
         'Content-Type': 'application/x-www-form-urlencoded'
-      }, data: {
+      },
+      data: {
         grant_type: 'client_credentials'
       }
     };
@@ -85,43 +44,47 @@ $('document').ready(function() {
       //console.log(response);
 
       var accessToken = response.access_token;
-      console.log(response.access_token);
-});
+      //console.log(response.access_token);
+
       $('.playlist-dropdown').on('click', function(event) {
         event.preventDefault();
+
+        var playlistSelected = $(this).attr('value');
+        //console.log(playlistSelected);
         var getPlaylist = {
           async: true,
           crossDomain: true,
-          url: 'https://api.spotify.com/v1/browse/categories/' + 'dinner' + '/playlists?limit=10',
+          url: 'https://api.spotify.com/v1/browse/categories/' + playlistSelected + '/playlists?limit=10',
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + accessToken + "'"}
+            Authorization: 'Bearer ' + accessToken + "'"
+          }
         };
 
         $.ajax(getPlaylist).done(function(data) {
-          console.log(data);
-          //console.log(JSON.stringify(data));
-          //console.log(data.playlists.items[Math.floor(Math.random() * 10) + 1]);
-          //console.log(data.playlists.items[Math.floor(Math.random() * 10) + 1].external_urls.spotify);
-          //console.log(data.playlists.items[Math.floor(Math.random() * 10)].uri);
+          //console.log(data);
 
           var playlistDiv = $('<iframe>');
           playlistDiv
             .attr(
               'src',
-              'https://open.spotify.com/embed?uri=' + data.playlists.items[Math.floor(Math.random() * 10)].external_urls.spotify)
+              'https://open.spotify.com/embed?uri=' +
+                data.playlists.items[Math.floor(Math.random() * 10)]
+                  .external_urls.spotify
+            )
             .css({
               width: '300',
               height: '380',
               frameborder: '0',
               allowtransparency: 'true',
             });
+          $('.playlist').html('');
           $('.playlist').append(playlistDiv);
           console.log(playlistDiv);
           //console.log('https://open.spotify.com/embed?uri=' + data.playlists.item[Math.floor(Math.random() * 10)].uri);
         });
       });
     });
-
+  });
 });
