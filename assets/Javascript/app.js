e1b9c191782e2be65cdf4c30ee54e71c1b2b27b5
Mike Ignaczak <mikeignaczak@gmail.com>
2018-01-25 12:09:55 -0600

    adding the rest

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,29 +1,23 @@
 $('document').ready(function() {
-<<<<<<< HEAD
-=======
 
   // ---------------------------------------------------------------------------
-  // Initialize Firebase--------------------------------------------------------
+  // Initialize Firebase
   var config = {
-    apiKey: 'AIzaSyCacnSxWt5028xJ6wkWR-1KeHNFbc8Ytws',
-    authDomain: 'papayas-project-1.firebaseapp.com',
-    databaseURL: 'https://papayas-project-1.firebaseio.com',
-    projectId: 'papayas-project-1',
-    storageBucket: 'papayas-project-1.appspot.com',
-    messagingSenderId: '795888444944',
+    apiKey: "AIzaSyAI2qL3fFcDg0LTKusDg6G6SLnqTWr6TUY",
+    authDomain: "login-3ab83.firebaseapp.com",
+    databaseURL: "https://login-3ab83.firebaseio.com",
+    projectId: "login-3ab83",
+    storageBucket: "",
+    messagingSenderId: "804779896412"
   };
   firebase.initializeApp(config);
-  var database = firebase.database();
   // ---------------------------------------------------------------------------
   // End Initialize Firebase----------------------------------------------------
 
   $("#4").hide();
   $("#5").hide();
-  $(".recipe-icon").hide();
-  localStorage.clear();
   // ---------------------------------------------------------------------------
   // Spotify Start--------------------------------------------------------------
->>>>>>> 1840b745aad0113f803402dd256bae45120b0495
   var playlistCategories = [
     'dinner',
     'romance',
@@ -31,11 +25,8 @@ $('document').ready(function() {
     'hiphop',
     'latin',
     'party',
-<<<<<<< HEAD
-=======
     'soul',
     'mood'
->>>>>>> 1840b745aad0113f803402dd256bae45120b0495
   ];
 
   for (i = 0; i < playlistCategories.length; i++) {
@@ -43,11 +34,7 @@ $('document').ready(function() {
       //console.log(playlistName);
       var newPlaylist = $('<div>');
       newPlaylist
-<<<<<<< HEAD
-        .addClass('dropdown-item playlist-dropdown')
-=======
         .addClass('playlist-dropdown topic-dropdown')
->>>>>>> 1840b745aad0113f803402dd256bae45120b0495
         .attr('value', playlistName)
         .html(playlistName);
       $('.playlist-list').append(newPlaylist);
@@ -56,11 +43,7 @@ $('document').ready(function() {
 
   $('.dropdown-toggle').on('click', function(event) {
     event.preventDefault();
-<<<<<<< HEAD
-    $('.playlist').html('');
-=======
     //$('.playlist').html('');
->>>>>>> 1840b745aad0113f803402dd256bae45120b0495
 
     //console.log('Dropdown Clicked');
 
@@ -76,58 +59,6 @@ $('document').ready(function() {
       data: {
         grant_type: 'client_credentials'
       }
-<<<<<<< HEAD
-    };
-
-    $.ajax(authSettings).done(function(response) {
-      //console.log(response);
-
-      var accessToken = response.access_token;
-      //console.log(response.access_token);
-
-      $('.playlist-dropdown').on('click', function(event) {
-        event.preventDefault();
-
-        var playlistSelected = $(this).attr('value');
-        //console.log(playlistSelected);
-        var getPlaylist = {
-          async: true,
-          crossDomain: true,
-          url: 'https://api.spotify.com/v1/browse/categories/' + playlistSelected + '/playlists?limit=10',
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + accessToken + "'"
-          }
-        };
-
-        $.ajax(getPlaylist).done(function(data) {
-          //console.log(data);
-
-          var playlistDiv = $('<iframe>');
-          playlistDiv
-            .attr(
-              'src',
-              'https://open.spotify.com/embed?uri=' +
-                data.playlists.items[Math.floor(Math.random() * 10)]
-                  .external_urls.spotify
-            )
-            .css({
-              width: '300',
-              height: '380',
-              frameborder: '0',
-              allowtransparency: 'true',
-            });
-          $('.playlist').html('');
-          $('.playlist').append(playlistDiv);
-          console.log(playlistDiv);
-          //console.log('https://open.spotify.com/embed?uri=' + data.playlists.item[Math.floor(Math.random() * 10)].uri);
-        });
-      });
-    });
-  });
-});
-=======
     };
 
     $.ajax(authSettings).done(function(response) {
@@ -195,6 +126,7 @@ $('document').ready(function() {
     'italian',
     'mexican',
     'spanish',
+    'middle\ eastern',
     'american',
     'southern',
     'german',
@@ -239,7 +171,6 @@ $('#recipe-button').on('click', function() {
                 };
                 $("#4").show();
                 $("#5").show();
-                $(".recipe-icon").show();
         },
         error: function(data) {
             console.log(data);
@@ -373,16 +304,15 @@ $('.dropdown-item').on('click', function(event) {
     var movieI = response.results[randomMovie].poster_path;
 
     localStorage.setItem("movie", response.results[randomMovie].id);
-    localStorage.setItem("favorite", false);
 
     var imgaeURL = 'http://image.tmdb.org/t/p/w185/';
 
     var imgURL = imgaeURL + movieI;
 
-    //console.log(imgURL);
+    console.log(imgURL);
 
     var moviePlot = response.results[randomMovie].overview;
-    //console.log(moviePlot);
+    console.log(moviePlot);
 
     // Creating a div to hold the movie title
     var title = $('<h1>').text(movieTitle);
@@ -402,158 +332,10 @@ $('.dropdown-item').on('click', function(event) {
 });
   // ---------------------------------------------------------------------------
   // Movies End-----------------------------------------------------------------
-
-  // ---------------------------------------------------------------------------
-
-  // Our Picks Table------------------------------------------------------------
-  var userDataRef = firebase.database().ref("favorites").orderByKey();
-  userDataRef.once("value").then(function(snapshot) {
-  snapshot.forEach(function(childSnapshot) {
-    var key = childSnapshot.key;
-    var childData = childSnapshot.val();               
-      // console.log(key)
-      // console.log(childData)
-    
-    var likes_val = childSnapshot.val().likes;
-    // console.log(likes_val)
-    
-    var name_val = childSnapshot.val().name;
-    // console.log(name_val)
-
-    $("#picksTable > tbody").append('<tr class="firebase-pick" id = "' + key + '"><td>' + name_val + '</td><td>' + likes_val + '</td><td>');
-
-    });
-  });  
-
-  // Populating a pick
-  $('#picksTable').on('click', '.firebase-pick', function(event) {
-    event.preventDefault();
-    var chosenKey = this.id;
-    var favoritesKey = database.ref("/favorites/" + chosenKey);
-    favoritesKey.once("value").then(function(snapshot) {
-      var favoritePlaylist = snapshot.val().playlist;
-      var favoriteRecipe = snapshot.val().recipe;
-      var favoriteMovie = snapshot.val().movie;
-      var favoriteName = snapshot.val().name;
-      var favoriteLikes = snapshot.val().likes;
-      $(".favorite-name").html("<h2 class='thank-you'>" + favoriteName + "</h2>");
-      localStorage.setItem("favorite", true);
-      localStorage.setItem("likes", favoriteLikes);
-      localStorage.setItem("key", chosenKey);
-      $("#4").show();
-      $("#5").show();
-      $(".recipe-icon").show();
-      var authSettings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token',
-        method: 'POST',
-        headers: {
-          Authorization: 'Basic NjIyYTUwNDZkN2ZkNDM2Njk4MGNhZDgyNDJiNTYzY2U6ZjA2YWNhNzUxNGI5NDI1ZDk3MTc3MGIzMDMwNGZkMzE=',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-          grant_type: 'client_credentials'
-        }
-      };
-      $.ajax(authSettings).done(function(response) {
-        var accessToken = response.access_token;
-        var getPlaylist = {
-          async: true,
-          crossDomain: true,
-          url: 'https://api.spotify.com/v1/users/' + favoritePlaylist,
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + accessToken + "'"
-          }
-        };
-        $.ajax(getPlaylist).done(function(data) {
-          console.log(data);
-          
-          var playlistDiv = $('<iframe>');
-          playlistDiv
-            .attr(
-              'src',
-              'https://open.spotify.com/embed?uri=' +
-                data
-                  .external_urls.spotify
-            )
-            .css({
-              width: '300',
-              height: '380',
-              frameborder: '0',
-              allowtransparency: 'true',
-            });
-          $('.playlist').html('');
-          $('.playlist').append(playlistDiv);
-        });
-      });
-      $.ajax({
-        method: 'GET',
-        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + favoriteRecipe + "/information?includeNutrition=false",
-        headers: { "X-Mashape-Key": 'n884na5hymmshNJQPRr9e1VpysDQp1p3GaVjsnkHuZgYB165gY', "Accept": 'application/json' },
-        success: function(data) {
-            console.log(data);
-            var recipe = data;
-            $(".title").html(recipe.title);
-            $(".recipe-picture").html("<img src='" + recipe.image + "'>");
-            $(".big-picture").html("<img src='" + recipe.image + "'>")
-            $(".cooking").html("<p>Ready In: " + recipe.readyInMinutes + " Minutes</p>");
-            var ingredients = recipe.extendedIngredients;
-                for (i = 0; i < ingredients.length; i++) {
-                  var ingredientList = $("<li>");
-                  ingredientList.html(ingredients[i].originalString);
-                  $(".ingredients").append(ingredientList);
-                };
-                var instructions = recipe.analyzedInstructions[0].steps;
-                for (j = 0; j < instructions.length; j++) {
-                    $(".instructions").append(" " + instructions[j].step);
-                };
-          },
-          error: function(data) {
-              console.log(data);
-          }
-      });
-      $('#title').html('');
-      $('.movieP').html('');
-      $('#plot').html('');
-      $.ajax({
-        url: 'https://api.themoviedb.org/3/movie/' + favoriteMovie + '?api_key=13fb7053b108732df8330eeb99f8a1f2',
-        method: 'GET',
-      }).done(function(response) {
-        console.log(response);
-        var movieTitle = response.original_title;
-        var movieI = response.poster_path;
-        var imgaeURL = 'http://image.tmdb.org/t/p/w185/';
-        var imgURL = imgaeURL + movieI;
-        var moviePlot = response.overview;
-        var title = $('<h1>').text(movieTitle);
-        var image = $('<img>').attr('src', imgURL);
-        image.addClass('picture-size');
-        var plot = $('<p>').text(moviePlot);
-        $('#title').prepend(title);
-        $('.movieP').prepend(image);
-        $('#plot').prepend(plot);
-        });
-      });
-      $('html, body').animate({ scrollTop: $('#2').offset().top }, 'slow');
-    });
-
-  // Voting Section
   $('.thumbs-up').on('click', function(event) {
     event.preventDefault();
     var name = $("#firebase-name").val();
-    var favorite = localStorage.getItem("favorite");
-    if (favorite === "true") {
-      var likes = localStorage.getItem("likes");
-      likes++;
-      var chosenKey = localStorage.getItem("key");
-      var favoritesKey = database.ref("/favorites/" + chosenKey);
-      favoritesKey.child("likes").set(likes);
-      $(".hide-me").html("<h2 class='thank-you'>Thanks For Your Feedback!!</h2>");
-    }
-    else if (favorite === "false" && name !== "") {
+    if (name !== "") {
       var newEntry = {
         name: name,
         playlist: localStorage.getItem("playlist"),
@@ -561,11 +343,11 @@ $('.dropdown-item').on('click', function(event) {
         movie: localStorage.getItem("movie"),
         likes: 1
       };
+
       database.ref("/favorites").push(newEntry);
-      $(".hide-me").html("<h2 class='thank-you'>Thanks For Your Feedback!!</h2>");
-    
-    }
-    else {};
+      $(".hide-me").html("<h2 class='recipe'>Great!!!</h2>");
+
+    };
   });
 
   $('.thumbs-down').on('click', function(event) {
@@ -575,8 +357,3 @@ $('.dropdown-item').on('click', function(event) {
 });
   // ---------------------------------------------------------------------------
   // Document Ready End---------------------------------------------------------
-<<<<<<< HEAD
->>>>>>> 1840b745aad0113f803402dd256bae45120b0495
-=======
-
->>>>>>> c91758f6f7be5146fe4f36614cc01f33d85eb0c2
---
+++ spotify-api-intro/client/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+//import $ from 'jQuery';
+
+const spotifyApi = new SpotifyWebApi();
+
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        albumArt: '',
+      },
+    };
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }
+
+
+  /*getNewPlaylist() {
+    spotifyApi.getCategoryPlaylists().then(response => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }*/
+
+  render() {
+    return <div className="App">
+        <a href="http://localhost:8888">
+          <button>Login with Spotify</button>
+        </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{height: 150}} />
+        </div>
+        <button onClick={() => this.getNowPlaying()}>
+          Check Now Playing
+        </button>
+      </div>;
+  }
+}
+
+export default App;
